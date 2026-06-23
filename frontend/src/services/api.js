d84d4e72cd51dc0532e07635d6e08299f00726bf
Mohit Chaurasiya <mohit.chaurasiya0857@gmail.com>
2026-06-23 10:40:23 +0530

    add vercel.json file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "https://smart-inventory-management-ixea.onrender.com/api",
+    // http://localhost:5000/api
+    // https://smart-inventory-management-ixea.onrender.com/api
 });
 
 api.interceptors.request.use((config) => {
